134d57bc9e51d59cfb2415186a59876b8512f19f
Selena Solis <lenansolis@gmail.com>
2019-05-29 19:35:51 -0500

    unlike button

---
+++ src/beers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './beerStyle.css';
+
+function Beers(props){
+    return(
+        <div className="beer">
+            <img src={props.image} alt={props.image}/>
+            <h2>{props.name}</h2>
+            <h3>{props.tagline}</h3>
+            <h3>{props.abv}</h3>
+            <button onClick={() => props.likeButton(props.index)}>{props.buttonText}</button>
+            
+        </div>
+    )
+}
+
+export default Beers
